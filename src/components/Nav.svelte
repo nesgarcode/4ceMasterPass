c21192d6f3e172f30c70ce7ac92116abcbd045ae
nesgarcode <55456895+nesgarcode@users.noreply.github.com>
2021-06-01 02:57:12 -0500

    Configuración inicial del proyecto
    Se agregaron las dependencias iniciales y se agrego una estructura de carpetas.

---
+++ src/components/Nav.svelte
@@ -0,0 +1,18 @@
+<nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="/">Inicio</a>
+        <a class="navbar-item" href="/">Sobre la MasterPass</a>
+        <a class="navbar-item" href="/">Contactanos</a>
+      </div>
+      <div class="navbar-end">
+        <a class="navbar-item" href="/">Inicia Sesion</a>
+        <a class="navbar-item" href="/">Registrate</a>
+      </div>
+    </div>
+  </nav>
